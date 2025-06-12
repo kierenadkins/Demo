7670a73a70d8793837d3f01063df3a8f4f735220
kierenadkins <75570969+kierenadkins@users.noreply.github.com>
2025-06-12 12:16:33 +0100

    add swagger

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -30,4 +30,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 app.Run();
